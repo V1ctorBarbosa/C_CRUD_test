c55bfaf77de742ea7f1fb901e88a5551302b515c
Victor <vicfer2708@gmail.com>
2024-04-29 11:29:35 -0300

    slip classes per file

---
+++ Dog.cs
@@ -0,0 +1,11 @@
+namespace CrudTest
+
+{
+    public class Dogs
+    {
+        public string? Name { get; set; } //são os proprety accessors. Eles estão dizer que você vai poder ver e setar a propriedade desse objeto
+        public int Age { get; set; }
+    }
+}
+
+
